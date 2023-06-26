0ef978d5bd93d7bec903c8fc80129c3d73533620
Kazuki SEKIGUCHI <kazu-10035@outlook.jp>
2023-06-27 00:55:04 +0900

    修正

---
+++ database/index.js
@@ -0,0 +1,5 @@
+window.localStorage.setItem("email", num);
+const email = window.localStorage.getItem("email");
+
+window.localStorage.setItem("password", num);
+const password = window.localStorage.setItem("password");
