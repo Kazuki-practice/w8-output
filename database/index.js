c6080a70aee013437cf741792db4248d3d01f422
Kazuki SEKIGUCHI <kazu-10035@outlook.jp>
2023-06-29 13:11:25 +0900

    jsファイル修正

--- database/index.js
+++ database/index.js
@@ -1,5 +1,23 @@
-window.localStorage.setItem("email", num);
-const email = window.localStorage.getItem("email");
+// window.localStorage.setItem("email", num);
+// const email = window.localStorage.getItem("email");
 
-window.localStorage.setItem("password", num);
-const password = window.localStorage.setItem("password");
+// window.localStorage.setItem("password", num);
+// const password = window.localStorage.setItem("password");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const emailInput = document.querySelector(".input[name='email']");
+  const passwordInput = document.querySelector(".input[name='password']");
+  const signInButton = document.querySelector(".button");
+
+  emailInput.value = localStorage.getItem("email") || "";
+  passwordInput.value = localStorage.getItem("password") || "";
+
+  signInButton.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    localStorage.setItem("email", emailInput.value);
+    localStorage.setItem("password", passwordInput.value);
+
+    this.closest("form").submit();
+  });
+});
